3b3ef1b00ccd42fe3793fdbb28014847fadfbb8b
“YJason-Woo1 git config --global user.name “Jason-Woo <4everjasonwoo@gmail.com>
2020-07-25 16:21:55 -0400

    solve several problems

---
+++ Permutations II/code.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+        for i in range(len(nums)):
+            if i == 0 or nums[i] != nums[i - 1]:
+                self.dfs(nums[:i] + nums[i + 1:], path + [nums[i]], res)
