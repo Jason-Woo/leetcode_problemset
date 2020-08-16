2fe402ecc8cbda62be78772072c222c9c8f05063
Jason <4everjasonwoo@gmail.com>
2020-08-15 22:16:13 -0400

    test

--- jz_problemset/jz5/code.py
+++ jz_problemset/jz5/code.py
@@ -1,6 +1,16 @@
 # -*- coding:utf-8 -*-
 class Solution:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
     def push(self, node):
         # write code here
+        self.stack1.append(node)
     def pop(self):
         # return xx
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        ans = self.stack2.pop()
+        while self.stack2:
+            self.stack1.append(self.stack2.pop())
+        return ans
