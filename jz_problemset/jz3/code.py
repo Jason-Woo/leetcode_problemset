1b9e08f26980e68b9dbc487510f372303c9ea112
Jason <4everjasonwoo@gmail.com>
2020-08-15 20:24:32 -0400

    test

--- jz_problemset/jz3/code.py
+++ jz_problemset/jz3/code.py
@@ -8,3 +8,12 @@ class Solution:
     # 返回从尾部到头部的列表值序列，例如[1,2,3]
     def printListFromTailToHead(self, listNode):
         # write code here
+        stack = []
+        tmp_node = listNode
+        while tmp_node is not None:
+            stack.append(tmp_node.val)
+            tmp_node = tmp_node.next
+        ans = []
+        while stack:
+            ans.append(stack.pop())
+        return ans
---
+++ jz_problemset/jz5/code.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def push(self, node):
+        # write code here
+    def pop(self):
+        # return xx
