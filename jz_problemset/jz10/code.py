a0f8f78f3293869152a079c62c22ec2cda2eddb8
Jason <4everjasonwoo@gmail.com>
2020-08-17 00:16:19 -0400

    test

--- jz_problemset/jz10/code.py
+++ jz_problemset/jz10/code.py
@@ -2,3 +2,11 @@
 class Solution:
     def rectCover(self, number):
         # write code here
+        if number <= 2:
+            return number
+        dp = [-1 for _ in range(number + 1)]
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, number + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[number]
