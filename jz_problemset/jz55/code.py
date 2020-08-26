2a9b9c0829f89b7721024234a5c6895d7b25018f
Jason <4everjasonwoo@gmail.com>
2020-08-26 19:11:07 -0400

    test

--- jz_problemset/jz55/code.py
+++ jz_problemset/jz55/code.py
@@ -6,3 +6,18 @@ class ListNode:
 class Solution:
     def EntryNodeOfLoop(self, pHead):
         # write code here
+        if pHead is None or pHead.next is None:
+            return None
+        node_fast = pHead.next.next
+        node_slow = pHead.next
+        while node_fast != node_slow:
+            if node_fast is None or node_fast.next is None:
+                return None
+            node_fast = node_fast.next.next
+            node_slow = node_slow.next
+        node_fast = pHead
+        while node_fast != node_slow:
+            node_fast = node_fast.next
+            node_slow = node_slow.next
+        return node_slow
+
