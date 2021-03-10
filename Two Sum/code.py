8c22875b458dd8720e4c43caf70a0a6c7a5fdb6b
Jason <4everjasonwoo@gmail.com>
2021-03-10 17:51:58 -0500

    test

--- Two Sum/code.py
+++ Two Sum/code.py
@@ -1,35 +1,15 @@
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
 class Solution(object):
-    def addTwoNumbers(self, l1, l2):
+    def twoSum(self, nums, target):
         """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
         """
-        result = ''
-        num = 0
-        while (l1 is not None and l2 is not None) or num != 0: #atten
-            if l1 is not None:
-                num += l1.val
-                l1 = l1.next
-            if l2 is not None:
-                num += l2.val
-                l2 = l2.next
-            if num >= 10:
-                result += str(num-10)
-                num = 1
-            else:
-                result += str(num)
-                num = 0
-        result = result[::-1]
-        p = ListNode(int(result[0]),None)
-        for i in range(1, len(result)):
-            tmp_node = ListNode(int(result[i]),p)
-            p = tmp_node
-        return p
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 
-
+s = Solution
+nums = [3, 2, 4]
+print(s.twoSum(s, nums, 6))
